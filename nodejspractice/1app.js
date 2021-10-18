9aa0b1b4e48855f229a31ae0f5d1c64921534bba
mehakkaur0409 <69787702+mehakkaur0409@users.noreply.github.com>
2021-10-18 17:26:46 +0530

    first class function and fn expr example added

--- nodejspractice/1app.js
+++ nodejspractice/1app.js
@@ -1,12 +1,27 @@
 //* first class functions-> function in js can be used as variables meaning it can be passed to a function param
 //* function expression-> function can be assigned to a variable and it is anonymous function. variable () -> will invoke the method.
 
-//method declaration
+//normal method declaration
 function printMehak() {
   console.log("mehak");
 }
-
-//method invoker
+//normal method invoker
 printMehak();
 
-//hello
+//!-----------------------------------
+//FCF
+ function methodAcceptingFn(fn){
+   fn();
+
+ }
+ //FCF method invoker
+ methodAcceptingFn(printMehak);
+
+ //!-----------------------------------
+
+ //function expression
+ var r=function(){
+   console.log('function expression method called');
+ }
+  //function expression method invoker
+  r();
