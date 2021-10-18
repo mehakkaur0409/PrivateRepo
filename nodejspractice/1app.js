52b6b6e6fb1def19a0773a8f9a6b1c272a61c164
mehakkaur0409 <69787702+mehakkaur0409@users.noreply.github.com>
2021-10-18 17:10:16 +0530

    nodejspractice folder added in private repo

---
+++ nodejspractice/1app.js
@@ -0,0 +1,12 @@
+//* first class functions-> function in js can be used as variables meaning it can be passed to a function param
+//* function expression-> function can be assigned to a variable and it is anonymous function. variable () -> will invoke the method.
+
+//method declaration
+function printMehak() {
+  console.log("mehak");
+}
+
+//method invoker
+printMehak();
+
+//hello
