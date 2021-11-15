9eaa3514e363976d9c40bce2d1dcac8b1ddff2ed
mehakkaur0409 <69787702+mehakkaur0409@users.noreply.github.com>
2021-11-15 22:48:16 +0530

    scopevchain with lexical env

--- nodejspractice/jspractice/index.js
+++ nodejspractice/jspractice/index.js
@@ -12,17 +12,37 @@
 // getname();
 
 //hoisting of three functions: meaning calling fns before they are declared.
-console.log(getname1);
-console.log(x);
-console.log(r);
-function getname1() {
-  var x = 5;
-  console.log("Java");
+
+// console.log(getname1);
+// console.log(x);
+// console.log(r);
+// function getname1() {
+//   var x = 5;
+//   console.log("Java");
+// }
+// var x = function () {
+//   var x = 5;
+//   console.log("Jvascript");
+// };
+// var r = () => {
+//   var x = 5;
+// };
+
+//Scope chain
+
+function childFunctionToGlobalContext() {
+  //hence can access m
+  var z = 10;
+  console.log(m);
+  childFunctionToAboveFunction();
+  function childFunctionToAboveFunction() {
+    console.log(z);
+  }
+
+  //   var x = () => {
+  //     console.log(m);
+  //   };
+  //   x();
 }
-var x = function () {
-  var x = 5;
-  console.log("Jvascript");
-};
-var r = () => {
-  var x = 5;
-};
+var m = 5;
+childFunctionToGlobalContext();
