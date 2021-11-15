adeaa946761526291549ade42a1daaf32f511fc5
mehakkaur0409 <69787702+mehakkaur0409@users.noreply.github.com>
2021-11-15 13:11:29 +0530

    js folder added

---
+++ nodejspractice/jspractice/index.js
@@ -0,0 +1,26 @@
+// getname(5);
+// console.log(getname());
+
+// //var x = 7;
+// function getname(r) {
+//   console.log("--->", r);
+//   console.log("Javascript");
+// }
+
+// //Funtion invoke
+// console.log(x);
+// getname();
+console.log(getname1);
+console.log(x);
+console.log(r);
+function getname1() {
+  var x = 5;
+  console.log("Java");
+}
+var x = function () {
+  var x = 5;
+  console.log("Jvascript");
+};
+var r = () => {
+  var x = 5;
+};
