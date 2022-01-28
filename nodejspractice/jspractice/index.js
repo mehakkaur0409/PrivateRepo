bd1d101bd5ff37c1fe83827b65b3e604011fd6d5
mehakkaur0409 <69787702+mehakkaur0409@users.noreply.github.com>
2022-01-29 00:05:55 +0530

    react class practice

--- nodejspractice/jspractice/index.js
+++ nodejspractice/jspractice/index.js
@@ -113,3 +113,11 @@ alert("Hello");
 
 // empty function syntax
 () => {};
+
+//------------>
+//React class
+class Car extends React.Component {
+  render() {
+    return <h2>Hi, I am a Car!</h2>;
+  }
+}
