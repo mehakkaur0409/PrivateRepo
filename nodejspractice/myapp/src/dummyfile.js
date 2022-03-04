e3278d07da6651c28d08e3246a3510412a29f2a7
mehakkaur0409 <69787702+mehakkaur0409@users.noreply.github.com>
2022-03-04 22:38:06 +0530

    constructor added

--- nodejspractice/myapp/src/dummyfile.js
+++ nodejspractice/myapp/src/dummyfile.js
@@ -5,6 +5,6 @@ class Car extends React.Component {
     this.state = {color: "red"};
   }
   render() {
-    return <h2>I am a Car!</h2>;
+    return <h2>I am a {this.state.color} Car!</h2>;
   }
 }
