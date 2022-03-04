23e45a6baf78f78a07f4d9160761d6814f5219d9
mehakkaur0409 <69787702+mehakkaur0409@users.noreply.github.com>
2022-03-04 22:51:59 +0530

    constructor added with state creation

--- nodejspractice/myapp/src/dummyfile.js
+++ nodejspractice/myapp/src/dummyfile.js
@@ -2,7 +2,12 @@
 class Car extends React.Component {
   constructor() {
     super();
-    this.state = {color: "red"};
+   this.state = {
+      brand: "Ford",
+      model: "Mustang",
+      color: "red",
+      year: 1964
+    };
   }
   render() {
     return <h2>I am a {this.state.color} Car!</h2>;
