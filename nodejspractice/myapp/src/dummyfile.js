881cc7d9332fa942f7a2aca9c09bab8382c58f2c
mehakkaur0409 <69787702+mehakkaur0409@users.noreply.github.com>
2022-03-04 22:54:22 +0530

    state access in render

--- nodejspractice/myapp/src/dummyfile.js
+++ nodejspractice/myapp/src/dummyfile.js
@@ -9,7 +9,17 @@ class Car extends React.Component {
       year: 1964
     };
   }
-  render() {
-    return <h2>I am a {this.state.color} Car!</h2>;
+    render ()
+    {
+        return(
+        <div>
+        <h1>My {this.state.brand}</h1>
+        <p>
+          It is a {this.state.color}
+          {this.state.model}
+          from {this.state.year}.
+        </p>
+      </div>)
+    };
   }
 }
