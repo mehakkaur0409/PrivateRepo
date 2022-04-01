403f9e1b108766e4a0cf9120636ad814f495cc99
mehakkaur0409 <69787702+mehakkaur0409@users.noreply.github.com>
2022-04-01 15:59:06 +0530

    google pay basic version added

---
+++ nodejspractice/google-pay/src/App.js
@@ -0,0 +1,54 @@
+import logo from './logo.svg';
+import './App.css';
+import GooglePayButton from '@google-pay/button-react';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+    
+      <h1>  <img src={ logo } className="App-logo" />google pay react demo</h1>
+      <hr />
+      <GooglePayButton
+        environment="TEST"
+  paymentRequest={{
+    apiVersion: 2,
+    apiVersionMinor: 0,
+    allowedPaymentMethods: [
+      {
+        type: 'CARD',
+        parameters: {
+          allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
+          allowedCardNetworks: ['MASTERCARD', 'VISA'],
+        },
+        tokenizationSpecification: {
+          type: 'PAYMENT_GATEWAY',
+          parameters: {
+            gateway: 'example',
+            gatewayMerchantId: 'exampleGatewayMerchantId',
+          },
+        },
+      },
+    ],
+    merchantInfo: {
+      merchantId: '12345678901234567890',
+      merchantName: 'Demo Merchant',
+    },
+    transactionInfo: {
+      totalPriceStatus: 'FINAL',
+      totalPriceLabel: 'Total',
+      totalPrice: '100.00',
+      currencyCode: 'USD',
+      countryCode: 'US',
+    },
+  }}
+  onLoadPaymentData={paymentRequest => {
+    console.log('load payment data', paymentRequest);
+      } }
+      />
+  
+    </div>
+  );
+}
+
+export default App;
